006f9df7c2ce39e2de9857a5748ff6d633f4b389
Michael Graves <michaelsgraves98@gmail.com>
2021-04-22 18:44:05 -0600

    I couldn't quite finish it

--- callbacks/practice.js
+++ callbacks/practice.js
@@ -29,6 +29,10 @@
 
 // Code Here 
 
+function first(array, callback){
+  callback(array[0])
+}
+
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
@@ -49,6 +53,10 @@ first(names, function(firstName){
 
 //Code Here
 
+function last(array, callback){
+  callback(array[array.length -1])
+}
+
 // Do not edit the code below.
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -66,6 +74,10 @@ last(names, function(lastName){
 */
 
 //Code Here
+
+function multiply(num1, num2, callback){
+  callback(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +97,17 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+
+function contains(array, name, callback){
+  for(i = 0; i < array.length; i++){
+    if (array[i] === name){
+      callback(true)
+    }
+    else{
+      callback(false)
+    }
+  }
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -107,6 +130,22 @@ contains(names, 'Colt', function(result){
 
 //Code Here
 
+function uniq(array, callback){
+  newArray = []
+  for(let i = 0; i < array.length; i++){
+    for(j = i + 1; j < array.length; j++){
+      // if (i === j){
+      //   splice(j, 1)
+      // }
+      if (i !== j){
+        newArray.push(i)
+      }
+    }
+  }
+  // callback(array)
+  callback(newArray)
+}
+
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -122,7 +161,13 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
+//Code Here
+
+function each(array, callback){
+  for(i = 0; i < array.length; i++){
+    callback(array[i], i)
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -135,11 +180,20 @@ each(names, function(item, indice){
 ////////// PROBLEM 7 //////////
 
 /*
-  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
+  Write a function called getUserById that takes in three parameters: an array of objects (users), 
+  an id and a callback, and searches for the user with a matching id.
   When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
 // Code here
+
+function getUserById(array, identification, callback){
+  for(i = 0; i < array.length; i++){
+    if (array.i.id === identification){
+      return callback(i)
+    }
+  }
+}
 
 // Do not edit the code below.
 var users = [
--- objects/practice.js
+++ objects/practice.js
@@ -16,6 +16,12 @@
 
 //Code here
 
+const me = {
+  name: 'Michael',
+  age: 22
+}
+
+
 ////////// PROBLEM 2 //////////
 
 /*
@@ -25,6 +31,15 @@
 
 //Code here
 
+const favoriteThings = {
+  band: 'Beatles',
+  food: 'Catfish',
+  person: 'Mary',
+  book: "Don't have one",
+  movie: 'Dark Knight',
+  holiday: 'Thanksgiving'
+}
+
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
@@ -32,12 +47,23 @@
 
 //Code here
 
+favoriteThings.car = 'Delorian'
+favoriteThings.brand = "Don't know the brand"
+
+// favoriteThings = {
+//   car: 'Delorian',
+//   brand: "Don't knoe the brand"
+// }
+
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
+
+favoriteThings.food = 'Chicken Nuggets'
+favoriteThings.book = 'Harry Potter'
 
 ////////// PROBLEM 3 //////////
 
@@ -51,11 +77,18 @@
 
 //Code here
 
+const backPack = {}
+
+const item = 'firstPocket'
+backPack.firstPocket = 'chapstick'
+
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
+
+
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -64,6 +97,8 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
+
+console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -85,6 +120,9 @@ var user2 = {
 */
 
 //Code Here
+
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
